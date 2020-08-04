2eb8a32a35d456535c3695387ad38b20f4ea403c
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-08-04 23:37:33 +0900

    コミット漏れ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+// 初期化
+admin.initializeApp();
+// 管理者権限でfirestoreを操作する
+// 毎回呼び出すと実行速度が遅くなるため、定数に入れてメモリを確保する
+const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
+  response.send({
+    statusCode,
+    body: JSON.stringify(body)
+  })
+}
+
+// 必ずexportして外部からAPIを叩けるようにする
+export const addDataset = functions.https.onRequest(async (req: any, res: any) => {
+  if (req.method !== 'POST') {
+    sendResponse(res, 405, {error: 'Invalid Request!'})
+  } else {
+    const dataset = req.body
+    // オブジェクトのキーを取り出す
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key]
+      await db.collection('questions').doc(key).set(data)
+    }
+    sendResponse(res, 200, {message: 'Successfully added dataset!'})
+  }
+})
